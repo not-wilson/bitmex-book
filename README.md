fa6dae39c5b15c860563cb6975d3b6d15e5726a9
notwilson <notwilson@protonmail.com>
2020-02-23 08:14:24 +1100

    The right one...

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # bitmex-book
-A small shared object to pass and store BitMEX socket API data.
+Process and store BitMEX socket API data.
+
+# Usage
+```javascript
+const book = new BitmexBook({ trim: { trade: 1000, chat: 1000, quote: 1000 }})
+```
+
+`trim` option isn't required and will be populated by default if omitted. It's the maximum size of any of the three specified tables. I've found the default seems to work fine. 10,000 quote, 1,000 chat and 1,000,000 trades isn't all that compromising on memory.
+
+To use, simply connect to the BitMEX API function, parse the message output and send the action to the relevant function.
+
+```javascript
+// Your socket handling code here.
+const book = new BitmexBook
+socket.on('message', data => {
+    data = JSON.parse(data)
+    // Extract useful data. data[1] is the global socket id.
+    const type      = data[0]
+    const stream    = data[2]
+    const reply     = data[3]
+
+    // Bind the stream ID to the reply so the library can track data easier.
+    reply._sid = stream
+
+    // Forward the data to the relevant function.
+    switch(reply.action) {
+        case 'partial': book.partial(reply);    break
+        case 'insert':  book.insert(reply);     break
+        case 'update':  book.update(reply);     break
+        case 'delete':  book.delete(reply);     break
+        default: throw new Error(`Unknown action '${reply.action}', API code outdated.`)
+    }
+})
+```
+
+To access the data, a `fetch(table, rows = 0, filter = null)` function has been included. `filter` operates exactly like `array.filter()`.
+
+```javascript
+// ** All socket and book handling code. **
+// Fetch all XBTUSD trades from the book.
+const trades = book.fetch('trades', 0, item => item.symbol === "XBTUSD")
+
+// Fetch the last 10 trades from Bitmex.
+const trades10 = book.fetch('trades', 10)
+
+// Filter all trades for the last 10 XBTUSD.
+const xbtusdTrades10 = book.fetch('trades', 10, item => item.symbol === "XBTUSD")
+```
+
+If you like my work, please let me know:
+notwilson@protonmail.com
+Find me in the BitMEX trollbox as notwilson
+
+A jesture of notice or a token of appreciation:
+ETH: 0xd9979f482da58b4432d0f52eb456f7dd1f4897e6
+BTC: 1HzR3Vyu231E8SsGLUbNYSb92bn6MGLEaV
+LTC: LTBHggmnrMACoB3JAH8rMy9r8hGxum7ZSw
+XRP: rBgnUKAEiFhCRLPoYNPPe3JUWayRjP6Ayg (destination tag: 536785858)
--- src/BitmexBook.js
+++ src/BitmexBook.js
@@ -1,4 +1,31 @@
-// Obejct to store and update BitMEX data.
+// Keep private to prevent accidental corruption of data.
+const books = {}
+const keys  = {}
+
+// Find the index of a specific piece of data in the book.
+const findTableIndex = (id, table, data) => {
+    const book = books[id]
+
+    // Find the index to update. Pass 0 back if the table has no keys to look for.
+    return !keys[table] ? 0 : book[table].findIndex(item => {
+
+        // Check data against keys for this table.
+        for(let i = 0; i < keys[table].length; i++) {
+            const key = keys[table][i]
+            if(item[key] !== data[key]) return false
+        }
+
+        // Return whether the result passed or not.
+        return true
+    })
+}
+
+// Convert tradeBin1d into trade etc.
+const getBaseTable = table => {
+    for(let i = 0; i < table.length; i++) if(table.charCodeAt(i) < 97 || table.charCodeAt(i) > 122) return table.substring(0, i)
+    return table
+}
+
 class BitmexBook {
     constructor(options = {}) {
         // Build the options menu.
@@ -7,109 +34,95 @@ class BitmexBook {
         // Default length of tables that will require trimming.
         if(!vars['trim']) options.trim = { quote: 10000, chat: 1000, trade: 1000000 }
 
-        // Look-up keys for tables.
-        const book  = {}
-        const keys  = {}
+        // Bind options.
+        this.options = options
+        
+        // Generate a random ID for data storage in globals to prevent multiple books from corrupting each-other.
+        this.id = Math.random().toString(36).substring(2)
+        
+        // Add this book to the global books object.
+        books[this.id] = {}
+    }
 
-        // Find the index of a specific piece of data in the book.
-        const findTableIndex = (table, data) => {
-            // Find the index to update. Pass -2 back if the table has no keys to look for.
-            return !keys[table] ? -2 : book[table].findIndex(item => {
+    // ***************** Handle BitMEX API Messages ***************** //
+    partial(reply) {
+        const book = books[this.id]
 
-                // Check data against keys for this table.
-                for(let i = 0; i < keys[table].length; i++) {
-                    const key = keys[table][i]
-                    if(item[key] !== data[key]) return false
-                }
+        // Store keys.
+        if(!keys[reply.table]) keys[reply.table] = reply.keys
 
-                // Return whether the result passed or not.
-                return true
+        // Add the table and data to the book.
+        if(!book[reply.table]) book[reply.table] = []
+
+        // If there's data to add, insert it.
+        if(reply.data && reply.data.length) this.insert(reply)
+    }
+
+    insert(reply) {
+        const book = books[this.id]
+
+        if(book[reply.table]) {
+            for(let i = 0; i < reply.data.length; i++) {
+                // Bind stream id to data.
+                reply.data[i]._sid = reply.stream   // Reply.stream is added via bitmex-socket, does not exist on the BitMEX API at this level. Does one level up.
+
+                // Add data to the book.
+                book[reply.table].push(reply.data[i])
+
+                // Remove first element every iteration the length is over max size.
+                const btable = getBaseTable(reply.table) // tradeBin1 into trade etc.
+                if(this.options.trim[btable] && (book[reply.table].length > this.options.trim[btable])) book[reply.table].shift()
+            }
+        }
+    }
+
+    update(reply) {
+        const book = books[this.id]
+
+        for(let i = 0; i < reply.data.length; i++) {
+            const data  = reply.data[i]
+            const index = findTableIndex(this.id, reply.table, data)
+
+            if(index >= 0) Object.keys(data).forEach(key => {
+                if(reply.table === "order" && data.leavesQty <= 0) book[reply.table].splice(index, 1)   // Remove cancelled/empty orders.
+                else if(book[reply.table][index]) book[reply.table][index][key] = data[key]             // Update the data row.
+                else book[reply.table].push(data)                                                       // Data doesn't exist, add it.
             })
+
+            else {} // thorw new Error("Don' fucked up.")
         }
+    }
 
-        // Convert tradeBin1d into trade etc.
-        const getBaseTable = table => {
-            for(let i = 0; i < table.length; i++) if(table.charCodeAt(i) < 97 || table.charCodeAt(i) > 122) return table.substring(0, i)
-            return table
+    delete(reply) {
+        const book = books[this.id]
+        for(let i = 0; i < reply.data.length; i++) {
+            const index = findTableIndex(this.id, reply.table, reply.data[i])
+            book[reply.table].splice(index, 1)
         }
+    }
 
-        // Initialize the book with any wanted data (used for echo server)
-        this.init = (ibook, ikeys) => {
-            // let is for the weak. Real men use const *all* the time.
-            Object.keys(ibook).forEach(key => book[key] = ibook[key])
-            Object.keys(ikeys).forEach(key => keys[key] = ikeys[key])            
-        }
+    // ***************** Make it Friendly ***************** //
+    // Get all data from the book.
+    all() { return { keys, book: books[this.id] } }
 
-        // BitMEX Socket Operations.
-        this.partial = reply => {
-            // Store keys.
-            if(!keys[reply.table]) keys[reply.table] = reply.keys
+    // Initiate a book with data from an already filled book.
+    initial(data) { 
+        books[this.id] = data.book
+        Object.keys(keys).forEach(key => keys[key] = data.keys[key])
+    }
 
-            // Add the table and data to the book.
-            if(!book[reply.table]) book[reply.table] = []
+    // Fetch all or the last x data from the book via table.
+    fetch(table, rows = 0, filter = null) {
+        const book      = books[this.id]
+        const target    = !filter ? book[table] : book[table].filter(filter)
+        const size      = (target.length - rows - 1) < 0 ? 0 : target.length - rows - 1
+        return target.slice(!rows ? 0 : size)
+    }
 
-            // If there's data to add, insert it.
-            if(reply.data && reply.data.length) this.insert(reply)
-        }
-
-        this.insert = reply => {
-            if(book[reply.table]) {
-                for(let i = 0; i < reply.data.length; i++) {
-                    // Bind stream id to data.
-                    reply.data[i]._sid = reply.stream   // Reply.stream is added via bitmex-socket, does not exist on the object elsewhere.
-
-                    // Add data to the book.
-                    book[reply.table].push(reply.data[i])
-
-                    // Remove first element every iteration the length is over max size.
-                    const btable = getBaseTable(reply.table) // tradeBin1 into trade etc.
-                    if(options.trim[btable] && (book[reply.table].length > options.trim[btable])) book[reply.table].shift()
-                }
-            }
-        }
-
-        this.update = reply => {
-            for(let i = 0; i < reply.data.length; i++) {
-                const data  = reply.data[i]
-                const index = findTableIndex(reply.table, data)
-
-                // Table doens't have any keys to find data for. Update the first row and move along. (connected table accepts this compromise.)
-                if(index === -2) {
-                    if(book[reply.table].length) book[reply.table][0] = data
-                    else book[reply.table].push(data)
-                }
-
-                // Update row data.
-                else if(index >= 0) Object.keys(data).forEach(key => {
-                    if(reply.table === "order" && data.leavesQty <= 0) book[reply.table].splice(index, 1) // Remove cancelled/empty orders.
-                    else book[reply.table][index][key] = data[key]
-                })
-
-                else {} // thorw new Error("Don' fucked up.")
-            }
-        }
-
-        this.delete = reply => {
-            for(let i = 0; i < reply.data.length; i++) {
-                const index = findTableIndex(reply.table, reply.data[i])
-                book[reply.table].splice(index, 1)
-            }
-        }
-
-        // Functions to access data within the book.
-        this.all    = (k = false) => k ? keys : book    // Get full book data.
-
-        // Fetch data from the book. filter is identical to a function passed to array.filter()
-        this.fetch  = (table, rows = 0, filter) => { 
-            const data = filter ? book[table].filter(filter) : book[table]
-            return (rows ? data.slice((data.length - rows) > 0 ? data.length - rows : data.length) : data)
-        }
-
-        // Flush data from the book.
-        this.flush = sid => {
-            // Remove all data from a specific stream ID from the book.
-            Object.keys(book).forEach(table => book[table] = book[table].filter(item => item._sid !== sid))
-        }
+    // Clear data from a book based on a stream ID (set in insert - stream id provided by @notwilson/bitmex-socket library).
+    flush(sid) {
+        const book = books[this.id]
+        Object.keys(book).forEach(key => book[key] = book[key].filter(item => { for(let i = 0; i < item.length; i++) return item[i]._sid !== sid } ))
     }
 }
 
